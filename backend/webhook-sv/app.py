9e28de0dadd6c08afe8565cbeb3d4859baae021e
13angs <13angz.um@gmail.com>
2022-11-03 15:35:19 +0700

    webhook-sv: dockerize and setup reverse proxy

--- backend/webhook-sv/app.py
+++ backend/webhook-sv/app.py
@@ -9,16 +9,23 @@ from flask import Flask
 from line_api import LineApi
 from dotenv import load_dotenv
 import os
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 # Load all env
 load_dotenv()
-DEBUG=os.environ['DEBUG']
+DEBUG=bool(os.environ['DEBUG'])
 
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(LineApi, '/api/v1/webhook')
+
+# Tell Flask it is Behind a Proxy
+if DEBUG == False:
+    app.wsgi_app = ProxyFix(
+        app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=DEBUG)
